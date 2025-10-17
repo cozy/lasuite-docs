3cd38ed8385fce18ac451dad16c7ccefcebd6547
Vince Linise <contact@vincelinise.com>
2025-10-17 15:13:51 +0200

    feat: Doc creation and router navigation from shell parent

--- src/frontend/apps/impress/src/layouts/MainLayout.tsx
+++ src/frontend/apps/impress/src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { css } from 'styled-components';
 
 import { Box } from '@/components';
@@ -8,6 +8,8 @@ import { HEADER_HEIGHT } from '@/features/header/conf';
 import { LeftPanel } from '@/features/left-panel';
 import { MAIN_LAYOUT_ID } from '@/layouts/conf';
 import { useResponsiveStore } from '@/stores';
+import { useCreateDoc } from '@/features/docs';
+import { useRouter } from 'next/router';
 
 type MainLayoutProps = {
   backgroundColor?: 'white' | 'grey';
@@ -19,10 +21,31 @@ export function MainLayout({
 }: PropsWithChildren<MainLayoutProps>) {
   const { isDesktop } = useResponsiveStore();
   const { colorsTokens } = useCunninghamTheme();
+  const router = useRouter();
+
   const currentBackgroundColor = !isDesktop ? 'white' : backgroundColor;
   const hasContent = !window._cozyBridge;
 
   const shouldShowLeftPanel = !window._cozyBridge;
+
+  const { mutate: createDoc, isPending: isCreatingDoc } = useCreateDoc({
+    onSuccess: (doc) => {
+      router.push(`/docs/${doc.id}`);
+    },
+  });
+
+  useEffect(() => {
+    window.onmessage = function (e) {
+      if (e.data == undefined || e.data == null || typeof e.data !== "string") return;
+      if (e.data.startsWith("newDoc")) {
+        createDoc();
+      }
+      if (e.data.startsWith("openFile:")) {
+        const fileId = e.data.split("openFile:")[1].trim();
+        router.push(`/docs/${fileId}`);
+      }
+    };
+  }, []);
 
   return (
     <Box className="--docs--main-layout">
